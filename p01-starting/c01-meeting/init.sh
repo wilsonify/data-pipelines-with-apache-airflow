107147db3c77c5873744eda5f10b1c264c2824d2
wilsonify <tom.andrew.wilson@gmail.com>
2025-05-05 08:28:24 -0400

    p01-starting/c01-meeting

--- p01-starting/c01-meeting/init.sh
+++
@@ -1,9 +0,0 @@
-airflow db upgrade && \
-sleep 5 && \
-airflow users create \
---username admin \
---password admin \
---firstname Anonymous \
---lastname Admin \
---role Admin \
---email admin@example.org
